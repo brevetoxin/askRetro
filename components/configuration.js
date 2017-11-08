0ae4dabbe4a57a4f642e572ab257beb8377639b8
Brian Darrow <brianpdarrow@gmail.com>
2017-11-08 18:30:18 -0500

    refactored into node module

--- components/configuration.js
+++ components/configuration.js
@@ -1,12 +1,33 @@
 'use strict'
 
-const configuration = require('../config/default.json');
+const appRoot = require('app-root-path');
+
+const defaultResourcePath = `${appRoot}/resources`;
+
+const config = {
+  resourcePath: defaultResourcePath
+};
 
 const get = (key) => {
-  if(configuration[key]) return configuration[key];
+  if (config[key]) return config[key];
   return;
 };
 
+const configure = (params) => {
+  config.resourcePath = params.resourcePath || defaultResourcePath;
+  config.startYear = params.startYear || '';
+  config.endYear = params.endYear || '';
+  config.team = params.team || '';
+  config.league = params.league || '';
+};
+
+const set = (key, value) => {
+  config[key] = value;
+};
+
 module.exports = {
-  get
+  get,
+  set,
+  configure,
+  config
 };
--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 'use strict';
 
 const eventBus = require('./components/eventBus');
-const config = require('./config/default.json');
+const configuration = require('./components/configuration');
 const fileManager = require('./components/fileManager');
 const processor = require('./components/processor');
 const log = require('./components/logger');
 
-return fileManager.retrieveFiles()
-  .then(files => {
-    const filePromises = files.map(file => processor.processFile(file));
-    return Promise.all(filePromises)
-  })
-  .catch(err => {
-    log.error(err);
-  })
+// const config = require('./config/default.json');
+// configuration.configure(config);
+
+const processFiles = () => {
+  return fileManager.retrieveFiles(configuration.config)
+    .then(files => {
+      const filePromises = files.map(file => processor.processFile(file));
+      return Promise.all(filePromises)
+    })
+    .catch(err => {
+      log.error(err);
+    });
+};
+
+module.exports = {
+  subscribe: eventBus.subscribe,
+  configuration,
+  processFiles
+};
